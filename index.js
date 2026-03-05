d4f5eb1a22b6fadf09089bdd4270e77405063edd
hubertDgomes <gomeshubert8@gmail.com>
2026-03-05 17:30:04 +0600

    refactor: update CORS origin to allow requests from production client

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import 'dotenv/config'
 
 app.use(express.json())
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://auth-client-drab.vercel.app",
     credentials : true
 }))
 
