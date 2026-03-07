2cfacf30223c0c75e01ee540fe30393c0c49ca83
hubertDgomes <gomeshubert8@gmail.com>
2026-03-07 21:27:09 +0600

    refactor: remove try-catch block for user persistence and email verification.

--- controllers/signupControllers.js
+++ controllers/signupControllers.js
@@ -1,6 +1,6 @@
 import userSchema from "../models/userSchema.js";
 import bcrypt from "bcrypt";
-import crypto from 'crypto'
+import crypto from "crypto";
 import emailVar from "../helper/emailVar.js";
 
 const signupControllers = async (req, res) => {
@@ -21,27 +21,22 @@ const signupControllers = async (req, res) => {
   }
 
   const otp = crypto.randomInt(100000, 999999).toString();
-  const expOtp = new Date (Date.now() + 10*60*1000)
+  const expOtp = new Date(Date.now() + 10 * 60 * 1000);
 
   bcrypt.hash(password, 10, async function (err, hash) {
     if (err) {
       return res.status(500).json({ msg: "Something went wrong!" });
     }
-    try {
-      const user = userSchema({
-        name,
-        email,
-        password: hash,
-        otp: otp,
-        expOtp,
-      });
-      await user.save();
-      await emailVar(email, otp, name);
-      res.json({ msg: "The data has been submitted" });
-    } catch (error) {
-      console.error("Signup error:", error.message);
-      res.status(500).json({ msg: "Failed to send verification email. Please try again." });
-    }
+    const user = userSchema({
+      name,
+      email,
+      password: hash,
+      otp: otp,
+      expOtp,
+    });
+    await user.save();
+    await emailVar(email, otp, name);
+    res.json({ msg: "The data has been submitted" });
   });
 };
 
